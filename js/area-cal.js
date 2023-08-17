1d198c49abe772585f1d04587f26c9ccca7ebcd0
TamannaTahsin1 <tamannatahsin648@gmail.com>
2023-08-18 01:43:30 +0600

    advance forms

--- js/area-cal.js
+++ js/area-cal.js
@@ -5,14 +5,25 @@ function calculateTriangleArea(){
 
     const area = 0.5 * baseField * heightField;
     setElementInnerText ('triangle-area', area);  
+
+    // add to calculation entry
+    addToCalculationEntry('triangle', area)
 }
 // for rectangle
 function calculateRectangleArea(){
     const widthField = getInputValue('rectangle-weight'); 
     const lengthField = getInputValue('rectangle-length');
 
+    // validation for width and length
+    if(isNaN(widthField) || isNaN(lengthField)){
+        alert('Please insert a number');
+        return;
+    }
     const area = widthField * lengthField;
     setElementInnerText('rectangle-area',area);
+
+    // add to calculation entry
+    addToCalculationEntry('rectangle', area)
 }
 // for  Parallelogram 
 function calculateParallelogramArea(){
@@ -21,6 +32,9 @@ function calculateParallelogramArea(){
 
     const area = base * height;
     setElementInnerText('parallelogram-area', area);
+
+    // add to calculation entry
+    addToCalculationEntry('parallelogram', area)
 }
 // for Rhombus
 function calculateRhombusArea(){
@@ -29,6 +43,9 @@ function calculateRhombusArea(){
 
     const area = 0.5* rhombusFirstArea * rhombusSecondArea;
     setElementInnerText('rhombus-area', area)
+
+    // add to calculation entry
+    addToCalculationEntry('rhombus', area)
 }
 // for Pentagon
 function calculatePentagonArea(){
@@ -37,6 +54,9 @@ function calculatePentagonArea(){
 
     const area = 0.5 * pentagonPerimeter * pentagonBase;
     setElementInnerText('pentagon-area', area);
+
+    // add to calculation entry
+    addToCalculationEntry('pentagon', area)    
 }
 // for ellipse 
 function calculateEllipseArea(){
@@ -46,6 +66,8 @@ function calculateEllipseArea(){
     const area = 3.14 * majorRadius * minorRadius;
     setElementInnerText('ellipse-area', area);
 
+        // add to calculation entry
+        addToCalculationEntry('ellipse', area)
 }
 // reuseable input value function (for all)
 function getInputValue(fieldId){
@@ -59,3 +81,17 @@ function getInputValue(fieldId){
     const element = document.getElementById(elementId);
     element.innerText = area;
     }
+// add to calculation entry
+function addToCalculationEntry(areaType, area){
+    console.log(areaType+ ' ' + area);
+    const calculationEntry = document.getElementById('calculation-entry');
+    
+    const count = calculationEntry.childElementCount;
+
+    const p =document.createElement('p');
+    p.classList.add('my-4');
+    p.innerHTML = `${count + 1}. ${areaType} ${area} cm<sup>2</sup>
+    <button class="btn btn-sm btn-success">Convert</button>`;
+    p.innerHTML =areaType+ ' ' + area; 
+    calculationEntry.appendChild(p);
+}
